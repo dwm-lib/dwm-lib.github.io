7979f82deeda75656b19887a2777a7695bf49208
dwm-lib <268713266+dwm-lib@users.noreply.github.com>
2026-03-18 17:18:51 -0500

    Add statistics footer

--- DWMLibrary.WebApp/Pages/HomePage.razor.cs
+++ DWMLibrary.WebApp/Pages/HomePage.razor.cs
@@ -2,19 +2,5 @@ namespace DWMLibrary.WebApp.Pages;
 
 public partial class HomePage
 {
-    private bool dataLoaded => (breeds is not null && breeds.Length > 0 && monsters is not null && monsters.Length > 0 && skills is not null && skills.Length > 0);
-    private bool notFound = false;
 
-    private Core.Models.Breed[]? breeds;
-    private Core.Models.Monster[]? monsters;
-    private Core.Models.Skill[]? skills;
-
-    protected override async Task OnInitializedAsync()
-    {
-        breeds = await DataService.GetBreedsAsync();
-        monsters = await DataService.GetMonstersAsync();
-        skills = await DataService.GetSkillsAsync();
-
-        notFound = (breeds is null || monsters is null || skills is null);
-    }
 }
