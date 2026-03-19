1715d1ee8c14052c41efd99525ccf85387c815dc
dwm-lib <268713266+dwm-lib@users.noreply.github.com>
2026-03-19 17:39:55 -0500

    Add Monster Image component

---
+++ DWMLibrary.WebApp/Components/Monsters/MonsterImageComponent.razor.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Components;
+
+namespace DWMLibrary.WebApp.Components.Monsters;
+
+public partial class MonsterImageComponent
+{
+    [Parameter]
+    public required Monster? monster { get; set; }
+
+    [Parameter]
+    public MonsterImageSize size { get; set; } = MonsterImageSize.Small;
+
+    public enum MonsterImageSize
+    {
+        Small = 1,
+        Medium = 2,
+        Large = 4,
+    }
+
+    public string css => size switch
+    {
+        MonsterImageSize.Large => "MonsterImage-4x",
+        MonsterImageSize.Medium => "MonsterImage-2x",
+        _ => "MonsterImage"
+    };
+
+    public string url => size switch
+    {
+        MonsterImageSize.Large when (monster is null) => "/pics/4x/wonderegg-4x.png",
+        MonsterImageSize.Medium when (monster is null) => "/pics/2x/wonderegg-2x.png",
+        _ when (monster is null) => "/pics/1x/wonderegg.png",
+        MonsterImageSize.Large => $"/pics/4x/{monster!.Name.ToLower()}-4x.png",
+        MonsterImageSize.Medium => $"/pics/2x/{monster!.Name.ToLower()}-2x.png",
+        _ => $"/pics/1x/{monster!.Name.ToLower()}.png"
+    };
+
+    public string alt => (monster?.Name.ToString() ?? "Logo") + " pic";
+
+    public int wh => size switch
+    {
+        MonsterImageSize.Large => 192,
+        MonsterImageSize.Medium => 96,
+        _ => 48
+    };
+}
